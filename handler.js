8f01a5d9b33dc9bc6059f366d7f7475c40715247
Jonathan Mejia <jonathanmejia.ar@gmail.com>
2021-08-16 19:47:28 -0300

    Add createUser lambda function

--- handler.js
+++ handler.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const querystring = require('querystring');
+
 module.exports.hello = async (event, context, callback) => {
   const response = {
     statusCode: 200,
@@ -17,6 +19,27 @@ module.exports.helloUser = async (event) => {
     body: JSON.stringify({
       message: `Hello ${event.pathParameters.name}!`,
       input: event
+    }),
+  };
+
+  return response;
+};
+
+module.exports.createUser = async (event) => {
+  const body = JSON.parse(event.body);
+
+  let user = {
+    id: 1,
+    username: body.username,
+    password: body.password,
+  };
+
+  const response = {
+    statusCode: 201,
+    body: JSON.stringify({
+      success: true,
+      message: `User has been created successfully!`,
+      data: user
     }),
   };
 
